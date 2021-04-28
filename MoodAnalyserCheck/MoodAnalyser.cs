35539ff42b2d13b935505b3fed9cf48c3f591906
Prashant Bhure <Prashantbhure50@gmail.com>
2021-04-28 22:44:34 +0530

    Add project files.

---
+++ MoodAnalyserCheck/MoodAnalyser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserCheck
+{
+    public class MoodAnalyser
+    {
+        private string message;
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+        public MoodAnalyser()
+        {
+        }
+        public string analyseMoodLive1()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+                if (this.message.Contains("Sad"))
+                    return "SAD";
+                else
+                    return "HAPPY";
+            }
+            catch (NullReferenceException)
+            {
+                return "happy";
+            }
+        }
+    }
+
+}
